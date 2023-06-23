472cfca4a5c873973b3e5e3258473d5d3a784af3
kisnak21 <krisnastya21@gmail.com>
2023-06-23 09:46:14 +0700

    initial project and structure ✨

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React = require('react');
+
+import {SafeAreaView, Text} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Text>Food Market App</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
